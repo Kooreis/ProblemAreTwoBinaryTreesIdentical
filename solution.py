425e91b6714f8e728e77909c58e58534cde25136
Kooreis <kooreis.official@gmail.com>
2024-05-18 02:45:47 -0500

    Added a function to check if two trees are identical. It recursively checks if the data in the nodes and their left and right children are the same.

--- solution.py
+++ solution.py
@@ -1,5 +1,8 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+def identicalTrees(a, b):
+    if a is None and b is None:
+        return True
+    if a is not None and b is not None:
+        return ((a.data == b.data) and
+                identicalTrees(a.left, b.left) and
+                identicalTrees(a.right, b.right))
+    return False
